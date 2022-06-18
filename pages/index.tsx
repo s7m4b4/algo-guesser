24314474c837a426aaeea7655c7321c4c13ccbcc
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-06-18 23:49:42 +1000

    added settings comp/provider to app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import GameStateProvider from "../context/GameState"
 import ScreenSwitcher from "../components/ScreenSwitcher";
+import SettingsProvider from "../context/Settings";
 
 export default function Index() {
-
   return (
     <div className="flex items-center justify-center h-screen p-12 bg-gradient-to-br from-indigo-900 via-blue-500 to-cyan-400">
       <GameStateProvider>
-        <ScreenSwitcher />
+        <SettingsProvider>
+          <ScreenSwitcher />
+        </SettingsProvider>
       </GameStateProvider>
     </div>
   );
