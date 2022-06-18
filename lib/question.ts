69de1fb0e63e8b37d3461bca64e9c69f35cb1054
s7m4b4 <8148044+s7m4b4@users.noreply.github.com>
2022-06-19 00:44:17 +1000

    updated qusetion with difficulty filter

--- lib/question.ts
+++ lib/question.ts
@@ -1,9 +1,28 @@
-export type Question = {
+export interface Question {
   title: string;
   titleSlug: string;
   content: string;
-  difficulty: string;
+  difficulty: Difficulty;
   topicTags: string[];
+}
+
+export type Difficulty = "Easy" | "Medium" | "Hard";
+
+export const questions: Array<Question> =
+  require("../data/questions.json") as Array<Question>;
+
+export const categories = questions.reduce(
+  (acc, question) => acc.concat(question["topicTags"]),
+  [] as string[]
+);
+
+export const uniqueCats = (question: Question) => {
+  categories.filter((item) => !question.topicTags.includes(item));
 };
 
-export const questions: Question[] = require("../data/questions.json");
+const unique = (question: Question, max: number) =>
+  [...new Set()]
+    .sort(() => 0.5 - Math.random())
+    .slice(0, Math.max(0, max - question.topicTags.length));
+
+export const totalQuestions = questions.length;
--- pages/api/question.ts
+++ pages/api/question.ts
@@ -1,10 +1,33 @@
-import { questions } from "../../lib/question";
+import { Question, questions } from "../../lib/question";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { Difficulty } from "../../@types/settings";
 
 const MAX_CHOICES = 10;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const question = questions[Math.floor(Math.random() * questions.length)];
+  const filteredQuestions: Question[] = [];
+  const { difficulty } = req.query;
+
+  if (difficulty != undefined) {
+    questions.map((question) => {
+      if (difficulty instanceof Array) {
+        if (difficulty.includes(question.difficulty)) {
+          filteredQuestions.push(question);
+        }
+      } else if (question.difficulty == (difficulty as Difficulty)) {
+        filteredQuestions.push(question);
+      }
+    });
+  }
+
+  let question: Question;
+  if (filteredQuestions.length > 0) {
+    question =
+      filteredQuestions[Math.floor(Math.random() * filteredQuestions.length)];
+  } else {
+    question = questions[Math.floor(Math.random() * questions.length)];
+  }
+
   const choices = questions.reduce(
     (acc, actual) => acc.concat(actual["topicTags"]),
     [] as string[]
@@ -19,7 +42,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   const response = {
     question: question,
-    choices: question.topicTags.concat(t),
+    choices: question.topicTags.concat(t).sort(() => 0.5 - Math.random()),
   };
 
   res.status(200).json(response);
