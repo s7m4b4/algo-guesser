091469b4af80033f1b809b4be708c4f6a0c5721e
Stephen B <8148044+s7m4b4@users.noreply.github.com>
2022-07-14 13:24:29 +1000

    revert-to-scss (#2)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '../styles/globals.scss';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
